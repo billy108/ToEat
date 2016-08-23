de465389bf2e9e4cdae919a2bc6cb0909fcd9f45
billy <billy_108@163.com>
2016-08-23 18:37:42 +0800

    增加沉浸式状态栏和标题栏

--- app/src/main/java/com/chuangsi/toeat/activities/MainActivity.java
+++ app/src/main/java/com/chuangsi/toeat/activities/MainActivity.java
@@ -1,11 +1,11 @@
 package com.chuangsi.toeat.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.chuangsi.toeat.R;
+import com.chuangsi.toeat.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
