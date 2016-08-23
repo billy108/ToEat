91909bb9f673c6ce2595ee381d56bd1261839e52
billy <billy_108@163.com>
2016-08-23 16:27:17 +0800

    创建项目

---
+++ app/src/main/java/com/chuangsi/toeat/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chuangsi.toeat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chuangsi.toeat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
